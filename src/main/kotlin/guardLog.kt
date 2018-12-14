659665fbdfeef43ede3d708a5f35e7629bb35cca
Mike Halma <mike_halma@yahoo.co.uk>
2018-12-14 20:06:17 +0000

    Load guards from log

--- src/main/kotlin/guardLog.kt
+++ src/main/kotlin/guardLog.kt
@@ -2,13 +2,49 @@ import java.time.LocalDateTime
 
 data class LogEntry(val time: LocalDateTime, val action: String)
 
+data class GuardDuty(val guard: Int)
+
+enum class GuardAction {
+    BEGINS_SHIFT, WAKES_UP, FALLS_ASLEEP;
+
+    companion object {
+        fun of(action: String): GuardAction {
+            return when {
+                action.contains("begins shift") -> BEGINS_SHIFT
+                action.contains("falls asleep") -> FALLS_ASLEEP
+                action.contains("wakes up") -> WAKES_UP
+                else -> throw IllegalArgumentException()
+            }
+        }
+    }
+}
+
 fun loadLogEntry(logEntryString: String): LogEntry {
-    val regex = """\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (.*)""".toRegex()
+    val regex = """\[(\d+)-(\d+)-(\d+) (\d+):(\d+)] (.*)""".toRegex()
     val result = regex.find(logEntryString)
     val (year, month, day, hour, minute, action) = result!!.destructured
     return LogEntry(LocalDateTime.of(year.toInt(), month.toInt(), day.toInt(), hour.toInt(), minute.toInt()), action)
 }
 
-fun loadLogEntries(logs: List<String>): Set<LogEntry> {
-    return logs.map {loadLogEntry(it)}.toSortedSet(compareBy {it.time})
+fun loadLogEntries(logs: List<String>): List<LogEntry> {
+    return logs.map {loadLogEntry(it)}.toSortedSet(compareBy {it.time}).toList()
+}
+
+fun getGuardDuties(logEntries: List<LogEntry>): List<GuardDuty> {
+    val guardDuties: MutableList<GuardDuty> = mutableListOf()
+    for (logEntry in logEntries) {
+        val guardAction = GuardAction.of(logEntry.action)
+        when (guardAction) {
+            GuardAction.BEGINS_SHIFT -> guardDuties.add(GuardDuty(getGuard(logEntry.action)))
+            else -> throw IllegalArgumentException()
+        }
+    }
+    return guardDuties
+}
+
+fun getGuard(action: String): Int {
+    val regex = """.*#(\d+).*""".toRegex()
+    val result = regex.find(action)
+    val (guard) = result!!.destructured
+    return guard.toInt()
 }
--- src/test/kotlin/DutyLogTest.kt
+++ src/test/kotlin/DutyLogTest.kt
@@ -1,5 +1,5 @@
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.core.Is
 import org.hamcrest.core.StringContains.containsString
 import org.junit.Test
 import java.time.LocalDateTime
@@ -9,7 +9,7 @@ class DutyLogTest {
     @Test
     fun loadLogEntry_simple() {
         assertThat(loadLogEntry("[1518-11-01 00:00] Guard #10 begins shift"),
-            Is.`is`(LogEntry(LocalDateTime.of(1518, 11, 1, 0, 0),
+            `is`(LogEntry(LocalDateTime.of(1518, 11, 1, 0, 0),
             "Guard #10 begins shift")))
     }
 
@@ -30,4 +30,32 @@ class DutyLogTest {
             assertThat(logEntry.action, containsString((index+1).toString()))
         }
     }
+
+    @Test
+    fun guardActionOf_correctEnum() {
+        assertThat(GuardAction.of("[1519-11-01 00:00] Guard #8 begins shift"), `is`(GuardAction.BEGINS_SHIFT))
+        assertThat(GuardAction.of("[1519-11-01 00:00] falls asleep"), `is`(GuardAction.FALLS_ASLEEP))
+        assertThat(GuardAction.of("[1519-11-01 00:00] wakes up"), `is`(GuardAction.WAKES_UP))
+    }
+
+    @Test
+    fun getGuard() {
+        assertThat(getGuard("Guard #1 begins shift"), `is`(1))
+        assertThat(getGuard("Guard #22 begins shift"), `is`(22))
+        assertThat(getGuard("Guard #333333 begins shift"), `is`(333333))
+    }
+
+    @Test
+    fun getGuardDuties_returnsGuards() {
+        val logs = listOf(
+            "[1518-11-01 00:00] Guard #2 begins shift",
+            "[1518-11-01 01:00] Guard #4 begins shift"
+            )
+        val guardDuties: List<GuardDuty> = getGuardDuties(loadLogEntries(logs))
+        assertThat(guardDuties.size, `is`(2))
+
+        assertThat(guardDuties[0].guard, `is`(2))
+        assertThat(guardDuties[1].guard, `is`(4))
+    }
+
 }
